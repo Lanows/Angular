15fa78175d5e1d7d162d6cd468ba2bcee537ad1d
Herculano Washington Silva <hercubaws@gmail.com>
2019-04-15 23:05:39 -0300

    Angular-Rotas

---
+++ app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { EditarComponent } from './editar/editar.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccountGuard } from './guards/account.guard';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'filmes', loadChildren: './movie/movie.module#MovieModule', canActivate: [AccountGuard] },
+  { path: 'series', loadChildren: './serie/serie.module#SerieModule', canActivate: [AccountGuard] },
+  { path: ':type/detalhes/:id/editar', component: EditarComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'filmes', canActivate: [AccountGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'filmes' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app/app.module.ts
@@ -0,0 +1,47 @@
+import { SerieService } from './serie/serie.service';
+import { MovieService } from './movie/movie.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AccountService } from './services/account.service';
+import { AccountGuard } from './guards/account.guard';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { LoginComponent } from './login/login.component';
+import { EditarComponent } from './editar/editar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EditarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+  ],
+  providers: [
+    AccountService,
+    AccountGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    MovieService,
+    SerieService
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
